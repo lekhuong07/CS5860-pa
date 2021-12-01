fbff3c089938bfb169efdbde6c2a82d8883be41b
Khuong Le <38338056+lekhuong07@users.noreply.github.com>
2021-11-30 22:35:44 -0800

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -27,23 +27,18 @@ def draw_graph(nodes, edges, back_edges, title=None, filename=None):
     dg.add_nodes_from(nodes)  # Add nodes to graph
     dg.add_edges_from(edges, color='b')  # Add edges to graph
     dg.add_edges_from(back_edges, color='r')
-    pos = nx.circular_layout(dg)
-    edges = dg.edges
-    colors = [dg[u][v]['color'] for u, v in edges]
 
-    val_map = {}
-    curr = 0
-    for n in nodes:
-        if n not in val_map:
-            val_map[n] = curr
-            curr += 0.5
+    # Get graph positions using Graphviz
+    #pos = nx.nx_agraph.graphviz_layout(dg)
+    pos = nx.nx_agraph.graphviz_layout(dg, prog="dot")
 
-    print("nodes ---", nodes, len(nodes))
-    print("edges ---", edges, len(edges))
-    print("edge colors ---", nodes, len(colors))
-
-    nx.draw(dg, with_labels=True, edge_color=colors, width=2, connectionstyle="arc3,rad=1", font_color='white')
-    plt.show()
+    # draw straight edges
+    nx.draw_networkx_labels(dg, pos)
+    nx.draw_networkx_edges(dg, pos, edgelist=edges, edge_color='r', arrows=True)
+    # draw curve edges
+    nx.draw_networkx_edges(dg, pos, edgelist=back_edges,
+                           connectionstyle=f"arc3, rad =1.5",
+                           edge_color='r', arrows=True)
 
     if title:
         plt.title(title)
@@ -131,7 +126,7 @@ class Graph:
 
             elif 'while' in line:
                 print("------3-------")
-                self.build_while(i, i)
+                self.build_while(i, 'while')
 
             elif 'do' in line:
                 print("------4-------")
@@ -145,11 +140,6 @@ class Graph:
                 print("------6--------")
                 self.build_assignment(i)
 
-            if i + 1 < len(list_input[i]):
-                if 'if' in self.list_input[i + 1] or 'else if' in self.list_input[i + 1] or 'else' in self.list_input[
-                    i + 1] \
-                        or 'while' in self.list_input[i + 1] or 'do' in self.list_input[i + 1]:
-                    self.curr_idx += 1
             i += 1
             print(self.mapping)
             print(self.graph)
@@ -169,9 +159,11 @@ class Graph:
         for key, value in self.graph.items():
             if self.mapping[key] == '}':
                 if key + 1 in self.mapping and key - 1 in self.mapping:
-                    if self.mapping[key - 1] == '}' and self.mapping[key + 1] == '}':
+                    if self.mapping[key - 1] == '}' and '}' in self.mapping[key + 1]:
                         if key not in self.graph[key - 1]:
                             self.graph[key - 1].append(key)
+
+        for key, value in self.graph.items():
             if type(key) == str:
                 temp = ''.join(key[:-1])
             else:
@@ -185,11 +177,21 @@ class Graph:
                     self.edges.append((key, v))
                 elif int(temp) > int(tempv):
                     self.back_edges.append((key, v))
-
         # take care of nodes
         for k in self.graph.keys():
             if len(self.graph[k]) != 0:
                 self.nodes.append(k)
+        tempe = []
+        tempbe = []
+        for e in self.edges:
+            if e[0] != e[1]:
+                tempe.append(e)
+
+        for be in self.back_edges:
+            if be[0] != be[1]:
+                tempbe.append(be)
+        self.edges = tempe
+        self.back_edges = tempbe
 
     def end_if_helper(self, flag):
         if len(self.if_statement) > 0:
@@ -197,8 +199,7 @@ class Graph:
             self.hashmap_if[t] = [self.max_visited]
             if flag == 'else if' or flag == 'else':  # just ending if statement and it gets to else if
                 ir = self.if_root.pop()
-                if self.max_visited + 1 not in self.graph[ir]:
-                    self.graph[ir].append(self.max_visited + 1)
+                self.graph[ir].append(self.max_visited + 1)
             else:  # ending else statement
                 temp = []
                 while len(self.if_statement) > 0:
@@ -210,30 +211,23 @@ class Graph:
                         if popidx not in temp:
                             temp.append(popidx)
                 self.if_statement = temp
-            if self.max_visited + 1 not in self.graph[self.max_visited]:
                 self.graph[self.max_visited].append(self.max_visited + 1)
-            if self.max_visited + 1 not in self.graph:
                 self.graph[self.max_visited + 1] = [-1]
 
     def end_while_helper(self):
         if len(self.while_loop) > 0:
             t = self.while_loop.pop()
-            if t not in self.graph[self.max_visited]:
-                self.graph[self.max_visited].append(t)
-            if self.max_visited + 1 not in self.graph[t]:
-                self.graph[t].append(self.max_visited + 1)
-            if self.max_visited + 1 not in self.graph:
-                self.graph[self.max_visited + 1] = [-1]
+            print("END WHILE---", t)
+            self.graph[self.max_visited].append(t)
+            self.graph[t].append(self.max_visited + 1)
+            self.graph[self.max_visited + 1] = [-1]
 
     def end_doloop_helper(self):
         if len(self.doloop) > 0:
             t = self.doloop.pop()
-            if t not in self.graph[self.max_visited]:
-                self.graph[self.max_visited].append(t)
-            if self.max_visited + 1 not in self.graph[self.max_visited]:
-                self.graph[self.max_visited].append(self.max_visited + 1)
-            if self.max_visited + 1 not in self.graph:
-                self.graph[self.max_visited + 1] = [-1]
+            self.graph[self.max_visited].append(t)
+            self.graph[self.max_visited].append(self.max_visited + 1)
+            self.graph[self.max_visited + 1] = [-1]
 
     def end_forloop_helper(self):
         if len(self.forloop) > 0:
@@ -241,8 +235,7 @@ class Graph:
             value = self.hash_for_loop[t][0]
             self.graph[value] = [t]
             self.graph[self.max_visited - 1] = [value]
-            if t not in self.graph[t]:
-                self.graph[t].append(self.max_visited)
+            self.graph[t].append(self.max_visited)
 
     def end_logical(self, i):
         self.curr_idx += 1
@@ -250,7 +243,6 @@ class Graph:
         self.graph[self.curr_idx] = [-1]
         self.max_visited = self.curr_idx
         if i in self.idx_end_if:
-            print("SOMETHING---", self.curr_idx, self.max_visited)
             if i + 1 < len(self.list_input):
                 if 'else if' in self.list_input[i + 1][1]:
                     self.end_if_helper('else if')
@@ -275,15 +267,18 @@ class Graph:
         self.graph[self.curr_idx] = [self.curr_idx + 1]
 
     def build_if_else(self, i):
-        print("GRAPH before IF ----", self.graph)
-        # self.curr_idx += 1
+        print("GRAPH before IF ----", self.graph, self.list_input[i - 1])
+        if i > 1 and 'for' in self.list_input[i - 1][1]:
+            self.curr_idx -= 1
+            if str(self.curr_idx - 1) + 'b' in self.graph:
+                self.graph[str(self.curr_idx - 1) + 'b'].pop()
+                self.graph[str(self.curr_idx - 1) + 'b'].append(self.curr_idx)
+        self.curr_idx += 1
         self.mapping[self.curr_idx] += self.list_input[i][1]
         self.graph[self.curr_idx] = [-1]
         print("CURRENT ----", self.curr_idx)
-        if self.curr_idx not in self.graph[self.max_visited]:
-            self.graph[self.max_visited].append(self.curr_idx)
-        if self.curr_idx + 1 not in self.graph[self.curr_idx]:
-            self.graph[self.curr_idx].append(self.curr_idx + 1)
+        self.graph[self.max_visited].append(self.curr_idx)
+        self.graph[self.curr_idx].append(self.curr_idx + 1)
         self.idx_end_if.append(self.find_end_index(i))
         if 'if' in self.list_input[i][1]:
             self.if_statement.append(self.curr_idx + 1)
@@ -291,21 +286,21 @@ class Graph:
         else:
             self.if_statement.append(self.curr_idx)
         self.max_visited = self.curr_idx
-        print("IDX ----", self.idx_end_if)
-        print("IF STATEMENT ----", self.if_statement)
-        print("HASHMAP if ---", self.hashmap_if)
-        print("GRAPH after IF ----", self.graph)
-        print("MAPPING after IF ----", self.mapping)
 
     def build_while(self, i, statement):
-        # self.curr_idx += 1
+        print("GRAPH before WHILE DO ----", self.list_input[i - 1], 'for' in self.list_input[i - 1][1], i)
+        self.curr_idx += 1
+        if i >= 1 and 'for' in self.list_input[i - 1][1]:
+            self.curr_idx -= 1
+            if str(self.curr_idx - 1) + 'b' in self.graph:
+                self.graph[str(self.curr_idx - 1) + 'b'].pop()
+                self.graph[str(self.curr_idx - 1) + 'b'].append(self.curr_idx)
         self.mapping[self.curr_idx] += self.list_input[i][1]
         self.graph[self.curr_idx] = [self.curr_idx + 1]
         if statement == 'while':
             self.idx_end_loop.append(self.find_end_index(i))
             # input in hashmap
             self.while_loop.append(self.curr_idx)
-
         else:
             self.idx_end_doloop.append(self.find_end_index(i))
             # input in logical
@@ -327,8 +322,7 @@ class Graph:
         statement = str(self.curr_idx) + 'c'
         temp = self.graph[self.curr_idx - 1]
         self.graph[self.curr_idx - 1] = [t for t in temp if t != self.curr_idx]
-        if condition not in self.graph[self.curr_idx - 1]:
-            self.graph[self.curr_idx - 1].append(condition)
+        self.graph[self.curr_idx - 1].append(condition)
         self.graph[statement] = [-1]
         self.graph[condition] = [-1]
         self.hash_for_loop[condition] = [statement]
@@ -336,18 +330,21 @@ class Graph:
         self.forloop.append(condition)
         self.curr_idx += 1
         self.graph[condition] = [self.curr_idx + 1]
-        #self.graph[self.curr_idx] = [-1]
+        self.graph[self.curr_idx] = [-1]
         self.max_visited = self.curr_idx
 
 
 if __name__ == "__main__":
     test_path = "testing.docx"
     pp = ProgParser(test_path)
-    l = pp.get_all_programs()['Program 13:']
+    l = pp.get_all_programs()['Program 14:']
 
     gr = Graph()
     gr.build_graph(l)
-    print("FINAL ---", gr.graph)
-    print("FINAL ---", gr.mapping)
     print(gr.list_input)
+    print("NODES ----", gr.nodes)
+    print("EDGES ----", gr.edges)
+    print("BACK_EDGES ----", gr.back_edges)
+    print("MAPPING ---", gr.mapping)
+    print("GRAPH ---", gr.graph)
     draw_graph(gr.nodes, gr.edges, gr.back_edges)
